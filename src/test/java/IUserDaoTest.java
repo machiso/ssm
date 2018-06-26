1daee30b72f12e3f25f628059b47e9f9cfd3f941
machi <machi@jcfc.cn>
2018-06-26 18:02:02 +0800

    ssm modify

--- src/test/java/IUserDaoTest.java
+++ src/test/java/IUserDaoTest.java
@@ -8,7 +8,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 // 加载spring配置文件
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration({"classpath:spring-mybatis.xml"})
+@ContextConfiguration({"classpath:applicationContext.xml"})
 public class IUserDaoTest {
 
     @Autowired
