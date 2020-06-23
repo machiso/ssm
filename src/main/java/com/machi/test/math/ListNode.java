e0fbe591bf37e54ae9b517e18e59cb0cc5d41eeb
machi <machi@jcfc.cn>
2020-06-23 18:51:15 +0800

    链表反转，合并，判断是否有环

--- src/main/java/com/machi/test/math/ListNode.java
+++ src/main/java/com/machi/test/math/ListNode.java
@@ -20,4 +20,30 @@ public class ListNode {
         }
         return pre;
     }
+
+    //判断链表是否有节点
+    public boolean ifHasCircle(ListNode head){
+        if (head == null || head.next == null) return false;
+        ListNode slow = head.next;
+        ListNode fast = head.next.next;
+        while (slow != fast){
+            if (fast == null || fast.next == null) return false;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return true;
+    }
+
+    //两个链表合并
+    public ListNode mergeListNode(ListNode l1,ListNode l2){
+        if (l1 == null) return l2;
+        if (l2 == null) return l1;
+        if (l1.val<l2.val){
+            l1.next = mergeListNode(l1.next,l2);
+            return l1;
+        }else {
+            l2.next = mergeListNode(l1,l2.next);
+            return l2;
+        }
+    }
 }
